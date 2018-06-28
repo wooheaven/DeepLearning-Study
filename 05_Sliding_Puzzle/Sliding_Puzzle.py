f1e273946a8d542ea129432dc1242505adb12a7a
rwoo <rwoo@bistel.com>
2018-06-29 08:30:49 +0900

    #56-rl-sp-3d_position ``` modified:   Sliding_Puzzle.py new file:   memo.txt new file:   plan.txt modified:   states.tsv ```

--- 05_Sliding_Puzzle/Sliding_Puzzle.py
+++ 05_Sliding_Puzzle/Sliding_Puzzle.py
@@ -1,5 +1,7 @@
 import psycopg2
 import numpy as np
+import json
+import copy
 
 
 def list_to_string(list):
@@ -24,7 +26,7 @@ def list2d_to_string(list2d):
 def string_to_np_array(string):
     data = [x for x in string]
     output = np.array(data, dtype=int)
-    return output.reshape(3,3)
+    return output.reshape(3, 3)
 
 
 def drop_table_if_exsits(conn, table_name):
@@ -51,7 +53,8 @@ def create_table_states(conn):
         query += "    state       VARCHAR(9)  NOT NULL, " + "\n"
         query += "    actions     VARCHAR(9)          , " + "\n"
         query += "    next_states VARCHAR(39)         , " + "\n"
-        query += "    next_ids    VARCHAR(27)           "
+        query += "    next_ids    VARCHAR(27)         , " + "\n"
+        query += "    position    json                  " + "\n"
         query += ")                                     "
         cursor.execute(query)
         conn.commit()
@@ -82,14 +85,15 @@ def check_state(conn, state):
             cursor.close()
 
 
-def insert_state(conn, state):
+def insert_state_position(conn, state, position):
     try:
         cursor = conn.cursor()
         is_state_duplicated = check_state(conn, state)
         if is_state_duplicated is False:
             state_str = list_to_string(state)
-            query = "INSERT INTO states (state) VALUES (%s)"
-            cursor.execute(query, (state_str,))
+            position_str = json.dumps(position)
+            query = "INSERT INTO states (state, position) VALUES (%s, %s)"
+            cursor.execute(query, (state_str, position_str))
             conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error Happen")
@@ -144,6 +148,24 @@ def select_state_by_id(conn, id):
             cursor.close()
 
 
+def select_state_position_by_id(conn, id):
+    try:
+        cursor = conn.cursor()
+        query = "SELECT state, position FROM states WHERE id = %s"
+        cursor.execute(query, (id,))
+        row = cursor.fetchone()
+        select_state = string_to_np_array(row[0])
+        select_position = row[1]
+        return select_state, select_position
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error Happen")
+        print(query)
+        print(error)
+    finally:
+        if cursor.closed is False:
+            cursor.close()
+
+
 def update_actions_by_id_state(conn, actions, id):
     try:
         actions_str = list_to_string(actions)
@@ -164,7 +186,7 @@ def update_actions_by_id_state(conn, actions, id):
             cursor.close()
 
 
-def update_next_state_by_id(conn, next_states, id):
+def update_next_states_by_id(conn, next_states, id):
     try:
         next_states_str = list_to_string(next_states)
         cursor = conn.cursor()
@@ -221,28 +243,28 @@ def update_next_id_by_id(conn, id, next_id_str):
             cursor.close()
 
 
-def get_actions(state):
-    i, j = get_target_number_position_list(state, 0)[0]
+def create_actions(state):
+    i, j = create_target_number_position_list(state, 0)[0]
     data = np.ones(9)
     actions = np.array(data, dtype=int).reshape(3, 3)
     actions[i][j] = 0
 
     # upper
     if 0 <= i - 1 <= 2 and 0 <= j <= 2:
-        actions[i-1][j] += 1
+        actions[i - 1][j] += 1
     # lower
     if 0 <= i + 1 <= 2 and 0 <= j <= 2:
-        actions[i+1][j] += 1
+        actions[i + 1][j] += 1
     # left
     if 0 <= i <= 2 and 0 <= j - 1 <= 2:
-        actions[i][j-1] += 1
+        actions[i][j - 1] += 1
     # right
     if 0 <= i <= 2 and 0 <= j + 1 <= 2:
-        actions[i][j+1] += 1
+        actions[i][j + 1] += 1
     return actions
 
 
-def get_target_number_position_list(state, target_num):
+def create_target_number_position_list(state, target_num):
     output = []
     for i in range(len(state)):
         for j in range(len(state[i])):
@@ -251,15 +273,23 @@ def get_target_number_position_list(state, target_num):
     return output
 
 
-def get_next_states(state, actions):
-    data = []
-    zero_i, zero_j = get_target_number_position_list(state, 0)[0]
-    for (action_i, action_j) in get_target_number_position_list(actions, 2):
-        next_state = np.copy(state)
-        next_state[zero_i][zero_j], next_state[action_i][action_j] = next_state[action_i][action_j], next_state[zero_i][zero_j]
-        data = np.append(data, next_state)
-    output = np.array(data, dtype=int).reshape(int(len(data)/9), 3, 3)
-    return output
+def create_next_states_next_positions(state, actions, position):
+    data_states = []
+    out_positions = []
+    zero_i, zero_j = create_target_number_position_list(state, 0)[0]
+    for (action_i, action_j) in create_target_number_position_list(actions, 2):
+        # state
+        tmp_state = np.copy(state)
+        tmp_state[zero_i][zero_j], tmp_state[action_i][action_j] = tmp_state[action_i][action_j], tmp_state[zero_i][
+            zero_j]
+        data_states = np.append(data_states, tmp_state)
+        # position
+        tmp_position = copy.deepcopy(position)
+        tmp_position["x"] += action_j - zero_j
+        tmp_position["y"] += zero_i - action_i
+        out_positions.append(tmp_position)
+    out_states = np.array(data_states, dtype=int).reshape(int(len(data_states) / 9), 3, 3)
+    return out_states, out_positions
 
 
 def pprint(data):
@@ -268,38 +298,59 @@ def pprint(data):
     print(data)
 
 
+def update_position_by_id(conn, id, position_json):
+    try:
+        cursor = conn.cursor()
+        query = ""
+        query += "UPDATE states         " + "\n"
+        query += "    SET position = %s " + "\n"
+        query += "    WHERE id = %s     " + "\n"
+        cursor.execute(query, (json.dumps(position_json), id))
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error Happen")
+        print(query)
+        print(error)
+    finally:
+        if cursor.closed is False:
+            cursor.close()
+
+
 if __name__ == "__main__":
     # initial data
     last_state_list = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-    last_state = np.array(last_state_list, dtype=int)
-    last_state_str = list_to_string(last_state)
-    last_state = string_to_np_array(last_state_str)
+    initial_state = np.array(last_state_list, dtype=int)
+    initial_state_str = list_to_string(initial_state)
+    initial_state = string_to_np_array(initial_state_str)
+    initial_position = json.loads('{"x": 2, "y": 0, "reword": 0}')
 
     # start
-    conn =  psycopg2.connect("dbname='mydatabase' user='myuser' host='localhost' port='65432' password='123qwe'")
+    conn = psycopg2.connect("dbname='mydatabase' user='myuser' host='localhost' port='65432' password='123qwe'")
 
     # initial database
     drop_table_if_exsits(conn, "states")
     create_table_states(conn)
-    insert_state(conn, last_state)
+    insert_state_position(conn, initial_state, initial_position)
 
     # state search
     id = find_next_id(conn)
     while id is not None:
-        # 1 state
-        select_state = select_state_by_id(conn, id)
+        # 1 state and position
+        state, position = select_state_position_by_id(conn, id)
 
         # 2 actions
-        actions = get_actions(select_state)
+        actions = create_actions(state)
         # pprint(actions)
         update_actions_by_id_state(conn, actions, id)
 
-        # 3 next_state
-        next_states = get_next_states(select_state, actions)
+        # 3 next_states
+        next_states, next_positions = create_next_states_next_positions(state, actions, position)
         # pprint(next_states)
-        update_next_state_by_id(conn, next_states, id)
-        for new_state in next_states:
-            insert_state(conn, new_state)
+        update_next_states_by_id(conn, next_states, id)
+        for i in range(len(next_states)):
+            new_state = next_states[i]
+            new_position = next_positions[i]
+            insert_state_position(conn, new_state, new_position)
 
         # 4 next_id
         next_id_str = ""
@@ -309,8 +360,6 @@ if __name__ == "__main__":
 
         # 5 id
         id = find_next_id(conn)
-
-        # 6
         if id is not None and id % 10000 == 0:
             print(id)
 
